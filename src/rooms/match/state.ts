3c089546dbbd2821b68672d8e96834269bfa46a2
taastaa1 <aaron.studer@swisscom.com>
2018-11-03 05:27:53 +0100

    test

--- src/rooms/match/state.ts
+++ src/rooms/match/state.ts
@@ -128,7 +128,6 @@ export class State {
     {
         var xCheck: boolean = false;
         var yCheck: boolean = false;
-        console.log(x < 10000 && x > 0);
         console.log(y < 5000 && y > 0);
         if(x < 10000 && x > 0)
         {
